029f872fbbbceaea89e8b9ea471c616e3f4a2332
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-12-05 19:49:38 +0530

    Add icons to dropdown-multiSelect component

---
+++ src/icons/chevron-down-icon.tsx
@@ -0,0 +1,20 @@
+export function IconChevronDown(isLoading: boolean) {
+    return (
+      <svg
+        className={`${isLoading ? "hidden" : "inline"} w-2.5 h-2.5 ml-2.5`}
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 10 6"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="m1 1 4 4 4-4"
+        />
+      </svg>
+    );
+  }
+  
