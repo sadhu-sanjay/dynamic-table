cc2702ea9162c8346d4086b1cad596b209c835bf
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-12-06 13:20:47 +0530

    fixed padding

--- src/components/Molecules/checkbox.tsx
+++ src/components/Molecules/checkbox.tsx
@@ -11,24 +11,22 @@ const Checkbox: React.FC<{
     onChange(!checked);
   };
 
-  const handleClick = () => {
-    alert("clicked");
-  };
-
   return (
     <div className=" border border-gray-200 rounded-lg dark:border-gray-600">
       <div className="flex items-center px-3">
         <input
-          id="vue-checkbox"
+          id={label}
           type="checkbox"
           value=""
           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+          onChange={handleCheckboxChange}
+          checked={checked}
         />
         <label
-          htmlFor="vue-checkbox"
-          className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          htmlFor={label}
+          className="w-full ps-1.5 py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
         >
-         {label || "Checkbox"} 
+          {label || "Checkbox"}
         </label>
       </div>
     </div>
