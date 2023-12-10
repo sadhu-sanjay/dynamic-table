1e00da47cbaf1a0b0bb45606176daac2c042b61c
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-12-10 19:50:31 +0530

    updated Sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,33 +5,39 @@ export default function DashboardLayout({
 }) {
   return (
     <>
-      <aside
-        className="fixed top-0 left-0 z-40 w-64 h-screen pt-14 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
-        aria-label="Sidenav"
-        id="drawer-navigation"
-      >
-        <div className="flex items-center justify-between h-14 px-4 bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-          <div className="flex items-center">
-            <button
-              aria-label="Toggle sidenav"
-              className="text-gray-500 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300 md:hidden"
-              id="sidenav-toggle"
-            >
-              <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M3 5C2.44772 5 2 5.44772 2 6C2 6.55228 2.44772 7 3 7H21C21.5523 7 22 6.55228 22 6C22 5.44772 21.5523 5 21 5H3ZM3 11C2.44772 11 2 11.4477 2 12C2 12.5523 2.44772 13 3 13H13C13.5523 13 14 12.5523 14 12C14 11.4477 13.5523 11 13 11H3ZM3 17C2.44772 17 2 17.4477 2 18C2 18.5523 2.44772 19 3 19H21C21.5523 19 22 18.5523 22 18C22 17.4477 21.5523 17 21 17H3Z"
-                ></path>
-              </svg>
-            </button>
+      <div className="flex bg-gray-100 dark:bg-gray-800">
+        <aside
+          className=" z-40 w-full sm:w-2/12 h-screen pt-14 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0  dark:bg-gray-800 dark:border-gray-700"
+          aria-label="Sidenav"
+          id="drawer-navigation"
+        >
+          <div className="flex items-center justify-between h-14 px-4 bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex items-center">
+              <button
+                aria-label="Toggle sidenav"
+                className="text-gray-500 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300 md:hidden"
+                id="sidenav-toggle"
+              >
+                <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M3 5C2.44772 5 2 5.44772 2 6C2 6.55228 2.44772 7 3 7H21C21.5523 7 22 6.55228 22 6C22 5.44772 21.5523 5 21 5H3ZM3 11C2.44772 11 2 11.4477 2 12C2 12.5523 2.44772 13 3 13H13C13.5523 13 14 12.5523 14 12C14 11.4477 13.5523 11 13 11H3ZM3 17C2.44772 17 2 17.4477 2 18C2 18.5523 2.44772 19 3 19H21C21.5523 19 22 18.5523 22 18C22 17.4477 21.5523 17 21 17H3Z"
+                  ></path>
+                </svg>
+              </button>
+            </div>
           </div>
-        </div>
-      </aside>
+        </aside>
 
-      <main className="h-[100dvh] w-[100dvw] bg-blue-800 flex flex-col justify-center items-center">
-        {children}
-      </main>
+        <main className="h-[100dvh] w-full sm:w-10/12 bg-blue-800 flex flex-col justify-center items-center">
+          {children}
+        </main>
+      </div>
+      {/* <div className="bg-red-200 w-screen h-screen flex ">
+        <div className="w-2/12 bg-red-900">Sanjay</div>
+        <div className="w-11/12 bg-blue-900">Paper</div>
+      </div> */}
     </>
   );
 }
--- src/components/Organisms/table-header.tsx
+++ src/components/Organisms/table-header.tsx
@@ -11,7 +11,40 @@ type TableHeaderProps = {
 const TableHeader: React.FC<TableHeaderProps> = ({ className }) => {
   return (
     <div className={`${className} w-full bg-white shadow-md dark:bg-gray-800`}>
-      <div className="flex flex-col items-center justify-between space-y-3 md:flex-row md:space-y-0 md:space-x-4"></div>
+      <button
+        data-drawer-target="drawer-navigation"
+        data-drawer-toggle="drawer-navigation"
+        aria-controls="drawer-navigation"
+        className="p-2 mr-2 text-gray-600 rounded-lg cursor-pointer md:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+      >
+        <svg
+          aria-hidden="true"
+          className="w-6 h-6"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <svg
+          aria-hidden="true"
+          className="hidden w-6 h-6"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <span className="sr-only">Toggle sidebar</span>
+      </button>
       <DynamicFilterList filterFields={fields} />
     </div>
   );
