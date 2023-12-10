c7cc313f59c8476472d8abab6b941a927c179082
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-12-10 10:04:19 +0530

    fixed full screen issue

--- src/components/Molecules/dropdown-multiSelect.tsx
+++ src/components/Molecules/dropdown-multiSelect.tsx
@@ -73,7 +73,7 @@ function DropdownMultiSelect({
           disabled={isLoading}
           onClick={toggleDropdown}
           type="button"
-          className=" rounded-4px py-2.5 px-5 text-sm font-medium text-gray-900 bg-white rounded-4pixel border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center"
+          className=" rounded-4px py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-4pixel border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center"
         >
           {selectedItems.length > 0 && (
             <span className="mr-2 inline-flex items-center justify-center w-4 h-4 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
