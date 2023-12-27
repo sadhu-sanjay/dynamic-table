f68e641390df68fd8499f1cfff368d5119885755
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-12-27 22:54:12 +0530

    updated random table

--- src/components/sections/table.tsx
+++ src/components/sections/table.tsx
@@ -67,7 +67,7 @@ export const Table: React.FC<TableProps> = ({
 
       {!error && !isFetching && data && data.length > 0 && (
         <>
-          <div className="w-[400px] h-[400px] ">
+          <div className="w-[60dvw] h-[83dvh] ">
             <table className=" text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
               <thead className=" sticky top-0 text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
