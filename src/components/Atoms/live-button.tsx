e794d44bc7bc787f1c3e2d1b2410f68ca0d05574
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-01-10 23:50:14 +0530

    updated dropdown

--- src/components/Atoms/live-button.tsx
+++ src/components/Atoms/live-button.tsx
@@ -2,9 +2,7 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 import { LIVE_EVENTS_URL } from "~/common/config";
 import { DocType } from "~/data/doc-data";
 import CircleIcon from "~/icons/circle-icon";
-import LiveNumberProvider, {
-  LiveNumberContext,
-} from "../../providers/live-event-provider";
+import { LiveNumberContext } from "~/providers/live-event-provider";
 
 type LiveButtonProps = {
   className?: string;
@@ -12,7 +10,15 @@ type LiveButtonProps = {
 };
 
 const LiveButton: React.FC<LiveButtonProps> = ({ className, doc }) => {
-  const liveNumber = useContext(LiveNumberContext);
+  const liveValue = useContext(LiveNumberContext);
+
+  const calculatedValue = (liveValue: any, id: number) => {
+    if (typeof liveValue === "number") {
+      return liveValue * id as number
+    } else {
+      return liveValue as string
+    }
+  };
 
   return (
     <div className="border-2 border-gray-600 rounded-full px-2 py-1 flex items-center gap-2 text-left text-xs text-gray-500 dark:text-gray-400">
@@ -24,7 +30,7 @@ const LiveButton: React.FC<LiveButtonProps> = ({ className, doc }) => {
         <CircleIcon />
       </div>
       <div className="text-xs font-semibold text-gray400 dark:text-gray-400">
-        {doc.live ? (liveNumber * doc.id) : "STABLE"}
+        {doc.live ? calculatedValue(liveValue, doc.id) : "STABLE"}
       </div>
     </div>
   );
--- src/components/Organisms/user-dropdown.tsx
+++ src/components/Organisms/user-dropdown.tsx
@@ -18,8 +18,45 @@ export default function UserDropdown({ session }: { session: Session }) {
     <div className="relative inline-block text-left">
       <Popover
         content={
-          <div className="w-full rounded-md bg-white p-2 sm:w-56">
-            <div className="p-2">
+          // <div className="w-full rounded-md bg-white dark:bg-gray-800 p-2 sm:w-56">
+          <div
+            className=" z-10 inline-block w-64 text-sm text-gray-500 transition-opacity 
+            duration-300 bg-white border-gray-200 rounded-lg shadow-sm 
+            dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600 p-2 "
+          >
+            <div className="p-3">
+        <div className="flex items-center justify-between mb-2">
+            <a href="#">
+                <Image width={40} height={40} className="w-10 h-10 rounded-full" src={image || `https://avatars.dicebear.com/api/micah/${email}.svg`} alt="Jese Leos"/>
+            </a>
+            <div>
+                <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-1.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Follow</button>
+            </div>
+        </div>
+        <p className="text-base font-semibold leading-none text-gray-900 dark:text-white">
+            <a href="#">{session?.user?.name}</a>
+        </p>
+        <p className="mb-3 text-sm font-normal">
+            <a href="#" className="hover:underline"> {session?.user?.email}</a>
+        </p>
+        <p className="mb-4 text-sm"> Building Software at <a href="https://sanjaygoswami.online" className="text-blue-600 dark:text-blue-500 hover:underline">sanjaygoswami.online</a>.</p>
+        <ul className="flex text-sm">
+            <li className="me-2">
+                <a href="#" className="hover:underline">
+                    <span className="font-semibold text-gray-900 dark:text-white">799</span>
+                    <span>Following</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" className="hover:underline">
+                    <span className="font-semibold text-gray-900 dark:text-white">3,758</span>
+                    <span>Followers</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div data-popper-arrow></div>
+            {/* <div className="p-2">
               {session?.user?.name && (
                 <p className="truncate text-sm font-medium text-gray-900">
                   {session?.user?.name}
@@ -28,7 +65,7 @@ export default function UserDropdown({ session }: { session: Session }) {
               <p className="truncate text-sm text-gray-500">
                 {session?.user?.email}
               </p>
-            </div>
+            </div> */}
             <button
               className="relative flex w-full cursor-not-allowed items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
               disabled
